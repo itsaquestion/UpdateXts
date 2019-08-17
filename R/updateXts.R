7f4763f5bbc7fcb232e8079f538ff0704e39d1a1
itsaquestion <itsaquestion@gmail.com>
2019-08-17 15:25:03 +0800

    fix docs

--- R/updateXts.R
+++ R/updateXts.R
@@ -2,15 +2,18 @@
 #'
 #' use new data to update an xts object.
 #'
-#' @param x old xts object
+#' @param object old xts object
 #' @param new_data an xts object with new data
+#' @param ... do nothing
 #'
 #' @return an updated xts object
 #' @export
 #' @import xts
 #' @import zoo
 #' @import checkmate
+#' @aliases update.xts
 #' @examples
+#' \dontrun{
 #' library(xts)
 #' a = as.xts(1:10, order.by = seq(Sys.Date() - 15, Sys.Date()-6, "days"))
 #' a
@@ -20,18 +23,19 @@
 #'
 #' updateXts(a,b)
 #' update(a,b)
+#' }
 
-updateXts = function(x, new_data){
-  assertClass(x,"xts")
+updateXts = function(object, new_data , ...){
+  assertClass(object,"xts")
   assertClass(new_data,"xts")
 
-  assertSameNames(x, new_data)
-  assertLength(x, new_data)
+  assertSameNames(object, new_data)
+  assertLength(object, new_data)
 
-  old_date = index(x)
+  old_date = index(object)
   new_date = index(new_data)
 
-  new_xts_p1 = x[!(old_date %in% new_date)]
+  new_xts_p1 = object[!(old_date %in% new_date)]
 
   suppressWarnings({
     new_xts =
@@ -43,9 +47,8 @@ updateXts = function(x, new_data){
 
 
 #' @export
-update.xts = function(object, new_data, ...){
-  updateXts(object, new_data)
-}
+#' @rdname updateXts
+update.xts = updateXts
 
 assertSameNames = function(x, y){
   #checkmate::assertClass(x,"xts")
